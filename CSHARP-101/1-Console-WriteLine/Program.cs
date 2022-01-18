aa9c2f6abf2734d9b75a18f445c5c2fab27af34d
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2022-01-18 23:57:07 +0300

    Csharp  Dersleri Console WriteLine Komutu İle İlk Program..

---
+++ CSHARP-101/1-Console-WriteLine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1_Console_WriteLine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello World !");
+
+        }
+    }
+}
