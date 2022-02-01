e94b88c7da1ae88841192df7dad72db759366bf4
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2022-02-01 12:50:49 +0300

    14-Datetime-Ve-Math-Siniflari

---
+++ CSHARP-101/14-Datetime-Ve-Math-Siniflari/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _14_Datetime_Ve_Math_Siniflari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine(DateTime.Now);//Günün tarihini getiriyor
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(30));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(10));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+            //Date Format
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));//24
+            Console.WriteLine(DateTime.Now.ToString("ddd"));//Sat
+            Console.WriteLine(DateTime.Now.ToString("dddd"));//Saturday
+
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));//04
+            Console.WriteLine(DateTime.Now.ToString("MMM"));//Apr
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));//April
+
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));//22
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));//2022
+
+            //Math kütüphanesi
+            Console.WriteLine(Math.Abs(-25));//25
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+            
+            
+            Console.WriteLine(Math.Ceiling(22.3));//23
+            Console.WriteLine(Math.Round(22.3));//22
+            Console.WriteLine(Math.Floor(22.7));//22
+
+
+            Console.WriteLine(Math.Max(2, 6));
+            Console.WriteLine(Math.Min(2, 6));
+
+            Console.WriteLine(Math.Pow(3,4));//3^4 =81
+            Console.WriteLine(Math.Sqrt(9));//karekök alır
+            Console.WriteLine(Math.Log(9));//logaritmasını alır
+            Console.WriteLine(Math.Exp(3));//e üzeri 3 ü verir
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CSHARP-101/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringChallenge("12:30pm - 12:00am");
+            
+            Console.ReadLine();
+
+        }
+
+        public static string StringChallenge(string str)
+        {
+
+            // code goes here
+
+            var tmpStr = str.Split('-');
+
+            var dt1StartDate = tmpStr[0];
+            var dt1EndDate = tmpStr[1];
+
+            DateTime dt1 = DateTime.Parse(dt1StartDate);
+            DateTime dt2 = DateTime.Parse(dt1EndDate);
+
+
+
+            //String.Format("{0:t}",dt1);
+            //String.Format("{0:t}",dt1EndDate);
+
+
+
+            // Console.WriteLine("--------------");
+            //Console.WriteLine(dt1StartDate);
+            //Console.WriteLine(dt1EndDate);
+            //Console.WriteLine("--------------");
+
+            TimeSpan dt11 =DateTime.ParseExact(dt1StartDate,"HH:mm tt",new DateTimeFormatInfo());
+            //DateTime dt2=DateTime.ParseExact(dt1EndDate,"HH:mm tt",new DateTimeFormatInfo());
+            TimeSpan timeSpan1 = new TimeSpan(dt11);
+            TimeSpan timeSpan2 = new TimeSpan(12, 0, 0);
+            var timeDiff = timeSpan2.Subtract(timeSpan1);
+
+
+            Console.WriteLine(dt1.ToString());
+
+
+            return str;
+
+        }
+    }
+}
