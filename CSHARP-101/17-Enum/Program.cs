04597876c5c057d87b248b5a58feb1eb5c8f80d4
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2022-02-07 14:21:21 +0300

    17-Enum

---
+++ CSHARP-101/17-Enum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _17_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int Sicaklik = 32;
+
+            if (Sicaklik <= (int)HavaSicakligi.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyin");
+            }
+            else if (Sicaklik >= (int)HavaSicakligi.Sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için sıcak bir gün");
+            }
+            else if (Sicaklik >= (int)HavaSicakligi.Normal && Sicaklik < (int)HavaSicakligi.CokSicak)
+            {
+                Console.WriteLine("Haydi dışarıya çıkalım");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        enum Gunler
+        {
+            Pazartesi,
+            Sali,
+            Carsamba,
+            Persembe,
+            Cuma,
+            Cumartesi,
+            Pazar
+
+
+
+        }
+
+        enum HavaSicakligi
+        {
+
+            Soguk = 5,
+            Normal = 20,
+            Sicak = 25,
+            CokSicak = 30
+
+        }
+    }
+}
