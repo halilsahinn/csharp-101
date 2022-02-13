2a5865ecfa169dd3ba19f65a36d6054d192de2da
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2022-02-13 22:24:37 +0300

    18-ArrayList

---
+++ CSHARP-101/18-ArrayList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Collection namepsace
+
+            ArrayList liste = new ArrayList();
+            //liste.Add("Ayşe");
+            //liste.Add(2);
+            //liste.Add(true);
+            //liste.Add('A');
+            //foreach (var item in liste)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //AddRange
+
+            Console.WriteLine("*****AddRange*****************");
+           // string [] renkler = {"mavi,Siyah,Beyaz" };
+            List<int> sayilar = new List<int>() {11,1,2,3,6,7,8,9 };   
+            
+           // liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Sort
+            Console.WriteLine("******Sort**********");
+            liste.Sort();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Binary Search
+
+            Console.WriteLine("*******Binary Search***");
+          
+            Console.WriteLine(liste.BinarySearch(9));
+
+
+            //Reverse
+
+            Console.WriteLine("********Reverse********");
+
+            liste.Reverse();
+            
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            //Clear
+            Console.WriteLine("*******Clear*******");
+            liste.Clear();
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CSHARP-101/AlgoritmaSorulari/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace AlgoritmaSorulari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************");
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+
+            try
+            {
+                int sayi =Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Lütfen {sayi} adet sayi giriniz");
+
+                for (int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine($"{i} nci sayısı giriniz");
+                    
+                    int tmpSayi=Convert.ToInt32(Console.ReadLine());
+                    if (tmpSayi % 2 == 0)
+                    {
+                        Console.WriteLine("Sayı Çift");
+                    }
+
+                }
+
+                
+
+                Console.WriteLine("************************");
+
+                Console.WriteLine("Lütfen iki tane pozitif sayı giriniz");
+
+                int n=Convert.ToInt32(Console.ReadLine()); 
+
+                int m=Convert.ToInt32(Console.ReadLine()); 
+
+
+                for (int i = 0;i <= n; i++)
+                {
+
+                    Console.WriteLine($"Lütfen{i} nci sayıyı giriniz");
+                    int tmpSayi = Convert.ToInt32(Console.ReadLine());
+
+                    if (tmpSayi == m || tmpSayi % 2==0)
+                    {
+
+                        Console.WriteLine($"M Sayısına Eşit yada Tam Bölüneni: {i}");
+                    }
+
+
+                }
+
+                Console.WriteLine("************************");
+
+                Console.WriteLine("Lütfen iki tane pozitif sayı giriniz");
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
