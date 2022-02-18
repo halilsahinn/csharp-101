ff6f28805425da44da6200b8e98b839f424dab1d
Halil ŞAHİN <halil.ibrahim.sahin@hotmail.com>
2022-02-18 13:35:36 +0300

    Algoritma Soruları

--- CSHARP-101/AlgoritmaSorulari/Program.cs
+++ CSHARP-101/AlgoritmaSorulari/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
 
 namespace AlgoritmaSorulari
 {
@@ -6,11 +10,13 @@ namespace AlgoritmaSorulari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("************************");
-            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+           Console.WriteLine("Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. " +
+           "Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.");
+           Console.WriteLine("Lütfen Bir Sayı Giriniz");
 
             try
             {
+                
                 int sayi =Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine($"Lütfen {sayi} adet sayi giriniz");
@@ -29,7 +35,8 @@ namespace AlgoritmaSorulari
 
                 
 
-                Console.WriteLine("************************");
+                Console.WriteLine("Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). " +
+                    "Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.");
 
                 Console.WriteLine("Lütfen iki tane pozitif sayı giriniz");
 
@@ -53,11 +60,52 @@ namespace AlgoritmaSorulari
 
                 }
 
-                Console.WriteLine("************************");
+                Console.WriteLine("Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin." +
+                    " Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın");
 
                 Console.WriteLine("Lütfen iki tane pozitif sayı giriniz");
+                List<string> tmpWords = new List<string>();
+                int tmpInputSayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Lütfen {0} adet kelime giriniz",tmpInputSayi);
+
+                string InputWord;
+                for (int i = 0; i < tmpInputSayi; i++)
+                {
+                    Console.WriteLine("Lütfen {0} nci kelimeyi giriniz",i);
+                    InputWord = Console.ReadLine();
+                    tmpWords.Add(InputWord);
+
+                }
+
+                tmpWords.Reverse();
+                tmpWords.ForEach(x => Console.WriteLine(x));
+
+            
+                
+                Console.WriteLine("Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. " +
+                    "Cümledeki toplam kelime ve harf sayısını console'a yazdırın");
+
+                Console.WriteLine("Lütfen Bir Cümle Yazın");
+
+                string InputSentence = Console.ReadLine();
+
+                
+                
+                int CharacterCount = 0;
+
+                int WordCount = InputSentence.Split(' ').ToList().Count;
+
+                Console.WriteLine("Kelime Sayısı: {0}",WordCount);
 
 
+                foreach (char item in InputSentence)
+                {
+                    CharacterCount++;
+                }
+
+               
+
+                Console.WriteLine("Harf Sayısı: {0}", CharacterCount);
 
             }
             catch (Exception ex)
@@ -69,5 +117,9 @@ namespace AlgoritmaSorulari
 
             Console.ReadLine();
         }
+
+      
+
+
     }
 }
